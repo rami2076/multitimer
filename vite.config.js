9fa319584e457d34298fa13190982c00b2499061
rami2076 <sakusennx@gmail.com>
2024-01-28 13:24:10 +0900

    spaからmulti page 対応

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import {resolve} from 'path';
+import {defineConfig} from 'vite';
 
-const root = resolve(__dirname, 'src')
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
     root,
+    build: {
+        outDir,
+        rollupOptions: {
+            input: {
+                sample1: resolve(root, 'multitimer', 'top', 'index.html'),
+                sample2: resolve(root, 'multitimer', 'sub', 'index.html'),
+            },
+        },
+    },
 })
